3857570c910273ce49fe24ad905856d7fced1750
MarcondesOliveira <marcondescurso@outlook.com>
2020-04-16 23:52:14 -0300

    Middleware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -21,7 +21,38 @@ app.use(express.json());
  * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
  */
 
+/**
+ * Middleware:
+ *
+ * Interceptador de requisições que pode interromper totalmente a requisição ou alterar dados da requisição
+ */
+
 const projects = [];
+
+function logRequests(req, res, next) {
+  const { method, url } = req;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next();
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: "Invalid project ID." });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use("/projects/:id", validateProjectId);
 
 app.get("/projects", (req, res) => {
   const { title } = req.query;
